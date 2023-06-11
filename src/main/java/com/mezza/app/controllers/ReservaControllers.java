d5853aa225b6ae27e385f5d654b9a50a0e377a4b
PriscilaNC <p.navarrete06@ufromail.cl>
2023-06-11 18:14:53 -0400

    conexion reserva

--- src/main/java/com/mezza/app/controllers/ReservaControllers.java
+++ src/main/java/com/mezza/app/controllers/ReservaControllers.java
@@ -4,6 +4,7 @@ import com.mezza.app.models.Reserva;
 import com.mezza.app.services.ReservaServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -13,12 +14,20 @@ public class ReservaControllers {
     @Autowired
     private ReservaServices reservaServices;
 
+    @GetMapping("/reserva/guardar")
+    public String reservar() {
+        return "reserva";
+    }
+
     @PostMapping("/reserva/guardar")
-    public String guardar(@RequestBody Reserva reserva) {
+    public String guardar(@RequestBody Reserva reserva, Model model) {
         try {
             reservaServices.guardar(reserva);
+            model.addAttribute("reserva", reserva);
         } catch (Exception e) {
+            System.out.println("a");
             return "ERROR";
+
         }
         return "redirect:/reserva-redirect";
     }
